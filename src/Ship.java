941e03fe5e5271f2f92ac87a678614ea5dcc9871
Stijn <stijn.wellens@telenet.be>
2013-03-01 08:53:53 +0100

    ship

--- src/Ship.java
+++ src/Ship.java
@@ -1,5 +1,21 @@
 
-//dit is een test
-public class Ship {
+/**
+ * @author Stijn
+ */
+public class Ship
+{
 
+	// position
+	private double x = 0;
+	private double y = 0;
+	
+	public int getXPosition()
+	
+	public int getYPosition()
+	
+	public int setXPosition()
+	
+	public int setYPosition()
+	
+	public int
 }
