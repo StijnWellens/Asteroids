5d1ef030966155fce750641941f6dbcd9ba46c99
Stijn Wellens <stijn.wellens@telenet.be>
2013-04-19 12:04:06 +0200

    doc ship

--- Asteroids/src/asteroids/model/Ship.java
+++ Asteroids/src/asteroids/model/Ship.java
@@ -1,11 +1,9 @@
 package asteroids.model;
 
-
-import asteroids.Util;
 import be.kuleuven.cs.som.annotate.*;
 
 /**
- * A class representing a ship with a position, velocity, direction and radius
+ * A class representing a ship with a specified position, velocity, direction, radius and mass.
  * 
  * @invar 	The x position of the ship must always be a valid x position. 
  * 			| isValidX(getX())
@@ -25,6 +23,10 @@ import be.kuleuven.cs.som.annotate.*;
  * 			| isValidRadius(getRadius())
  * @invar	The mass of the ship must always be a valid mass.
  * 			| isValidMass(getMass())
+ * @invar	The thruster of the ship must always be a valid thruster.
+ * 			| isValidThruster(getThruster())
+ * @invar	Each ship must have a proper world.
+ * 			| hasProperWorld()
  * @author 	Julie Wouters & Stijn Wellens
  * 			Students Bachelor of Science in Engineering 
  * 			(Computer Science and electrical engineering)
@@ -34,16 +36,59 @@ import be.kuleuven.cs.som.annotate.*;
  */
 public class Ship extends SpaceObject{
 
-	
-	public Ship() {
+	/**
+	 * Initializes this new default ship.
+	 * 
+	 * @effect	This ship is initialized as a default SpaceObject.
+	 * 			| super()
+	 * @effect	The direction of this Ship is set to PI/2.
+	 * 			| setDirection(PI/2)
+	 *  @effect	The mass of this new ship will be set to 1 kg.
+	 * 			| setMass(mass)
+	 * @effect	The thruster of this new ship will be set to a thruster 
+	 * 			which is not enabled and has a powerOutput of 1.1E18 kiloNewton.
+	 * 			| setThruster(false, 1.1E18)
+	 */
+	public Ship() throws IllegalArgumentException {
 		super();
 		setDirection(PI / 2);
+		setMass(1);
+		setThruster(false, 1.1E18);
 	}
 	
+	/**
+	 * Initializes this new ship with the given x and y position components, the given x and y velocity components,
+	 * the given radius, the given direction and the given mass.
+	 * 
+	 * @param 	x
+	 *        	The initial x position for this new Ship.
+	 * @param 	y
+	 *        	The initial y position for this new Ship.
+	 * @param 	xVelocity
+	 *        	The initial x velocity component for this new Ship.
+	 * @param 	yVelocity
+	 *        	The initial y velocity component for this new Ship.
+	 * @param 	radius
+	 *        	The radius of this new Ship.
+	 * @param	angle
+	 * 			The initial direction of this new Ship.
+	 * @param 	mass
+	 * 			The mass of this new Ship.
+	 * @effect	This new ship is initialized as a SpaceObject with the given x and y position components,
+	 * 			the given x and y velocity components and the given radius.
+	 * 			| super(x, y, xVelocity, yVelocity, radius)
+	 * @effect	The direction of this new ship will be set to valid multiple of the given angle.
+	 * 			| setDirection(makeAngleValdi(angle))
+	 * @effect	The mass of this new ship will be set to the given mass.
+	 * 			| setMass(mass)
+	 * @effect	The thruster of this new ship will be set to a thruster 
+	 * 			which is not enabled and has a powerOutput of 1.1E18 kiloNewton.
+	 * 			| setThruster(false, 1.1E18)
+	 */
 	public Ship(double x, double y, double xVelocity, double yVelocity,
-			double radius, double angle, double mass) {
+			double radius, double angle, double mass) throws IllegalArgumentException {
 		super(x, y , xVelocity, yVelocity,radius);
-		setDirection(angle);
+		setDirection(makeAngleValid(angle));
 		setMass(mass);
 		setThruster(false, 1.1E18);
 	}
@@ -51,15 +96,15 @@ public class Ship extends SpaceObject{
 	// direction: nominal programming
 	
 	private static final double	PI	= Math.PI;
-	private double				direction;
+	private double	direction; // in radians
 	
 	/**
 	 * Returns the angle of the direction of this spaceObject.
 	 * 
-	 * @return the angle of the direction of the spaceObject | this.direction
+	 * @return 	the angle of the direction of the spaceObject 
+	 * 			| this.direction
 	 */
 	@Basic
-	@Raw 
 	public double getDirection() {
 		return this.direction;
 	}
@@ -68,7 +113,7 @@ public class Ship extends SpaceObject{
 	 * Returns whether the given direction is valid or not.
 	 * 
 	 * @param angle
-	 *            The angle in which the spaceObject moves.
+	 *        The angle in which the spaceObject moves.
 	 * @return True if and only if the given angle is a number and is between
 	 *         zero and 2*Pi. 
 	 *         | result == ((!Double.isNaN(angle)) && (angle >=0) && (angle < 2*PI))
@@ -78,13 +123,13 @@ public class Ship extends SpaceObject{
 	}
 
 	/**
-	 * Set the direction of the spaceObject to the given direction.
+	 * Sets the direction of the spaceObject to the given direction.
 	 * 
 	 * @param angle
-	 *            The angle of the given direction.
+	 *        The angle of the given direction.
 	 * @pre The given direction must be a valid direction. 
 	 * 		| isValidDirection(angle)
-	 * @post The new direction of the spaceObject is equal to the modulo 2*Pi of the given direction of the spaceObject. 
+	 * @post The new direction of this spaceObject is equal to the given direction. 
 	 * 		| (new this).getDirection() == angle
 	 */
 	public void setDirection(double angle) {
@@ -98,16 +143,16 @@ public class Ship extends SpaceObject{
 	 * Turns the spaceObject with a given angle.
 	 * 
 	 * @param angle
-	 *            The angle that needs to be added to the current direction.
+	 *        The angle that needs to be added to the current direction.
 	 * @pre The sum of the old direction and the given angle must be a valid direction. 
 	 * 		| isValidDirection(this.getDirection()+angle)
 	 * @effect The direction of the spaceObject is set to the sum of the current direction and the given angle. 
 	 * 		| (new this).setDirection(this.getDirection() + angle)
 	 */
 	public void turn(double angle) {
-		assert isValidDirection(getDirection() + angle);
+		assert isValidDirection(angle);
 
-		setDirection(getDirection() + angle);
+		setDirection(makeAngleValid(getDirection() + angle));
 	}
 	
 	/**
@@ -140,18 +185,29 @@ public class Ship extends SpaceObject{
 		}
 	}
 	
+	// Thruster
+	
+	private Thruster thruster;	
+	
 	public Thruster getThruster()
 	{
 		return this.thruster;
 	}
 	
-	public void setThruster(boolean enabled, double powerOutput) throws IllegalArgumentException
+	public boolean isValidThruster(Thruster thruster)
 	{
-		this.thruster = new Thruster(enabled, powerOutput);		
+		return (thruster != null);
 	}
 	
-	private Thruster thruster;	
-	
+	public void setThruster(boolean enabled, double powerOutput) throws IllegalArgumentException
+	{
+		Thruster thruster = new Thruster(enabled, powerOutput);		
+		
+		if(!isValidThruster(thruster))
+			throw new IllegalArgumentException();
+		
+		this.thruster = thruster;
+	}		
 	
 	public void thrust(double time) {
 		if( !Double.isNaN(time) && !Double.isInfinite(time) && time >=0)
